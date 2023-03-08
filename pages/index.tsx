71e2e9078a53c5eabff16b9c50e965b90804878e
moyashiharusamen <moyashi.harusamen@gmail.com>
2023-03-08 18:15:43 +0900

    Next.jsで猫画像ジェネレーターを作ろう : 画像を取得する関数を実装する

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,3 +5,10 @@ const IndexPage: NextPage = () => {
 }
 
 export default IndexPage;
+
+const fetchImage = async () => {
+  const res = await fetch('https://api.thecatapi.com/v1/images/search');
+  const images  = await res.json();
+  console.log(images);
+  return images[0];
+};
