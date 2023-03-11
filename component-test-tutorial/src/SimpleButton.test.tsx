0aa63eab8fd67c684aa85a2ddf5c5420b09985f4
moyashiharusamen <moyashi.harusamen@gmail.com>
2023-03-12 00:13:01 +0900

    Reactコンポーネントのテストを書こう : テスト対象のコンポーネントを作る

---
+++ component-test-tutorial/src/SimpleButton.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen, act } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { SimpleButton } from "./SimpleButton";
+
+test('ボタンをクリックするとON/OFFの表示が切り替わる', () => {
+    render(<SimpleButton />);
+    const simpleButton = screen.getByRole('button');
+    expect(simpleButton).toHaveTextContent('OFF');
+    act(() => {
+        userEvent.click(simpleButton);
+    });
+    expect(simpleButton).toHaveTextContent('ON');
+});
